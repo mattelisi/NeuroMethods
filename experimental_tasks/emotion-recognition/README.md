7dc4134ced083d42b74ac7f6f6d0527a5071923b
Matteo Lisi <matte.crash@gmail.com>
2022-09-16 14:48:10 +0100

    added emotion recognition experiment

---
+++ experimental_tasks/emotion-recognition/README.md
@@ -0,0 +1,15 @@
+## Emotion recognition task
+
+This task require participants to indicate whether a morphed face appear sad or happy. The stimuli and approach are similar to those used by Michalek et al (2021)
+
+### Instructions
+
+In order to run, make sure the script `emrec00.m` and the folders 'img' and 'functions' are in the current working directory. You may also want to check that the screen settings (monitors width and viewing distance); these are currently set to these defaults
+
+In order to run this type `emrec00` in the coomand line. You will be then asked to type in a participant ID code and whether tyou would like to use the female or male actors as stimuli.
+
+In the task, you can respond by pressing 'H' for happy and 'S' for sad.
+
+### References
+
+Michalek, J. E., Lisi, M., Awad, D., Hadfield, K., Mareschal, I., & Dajani, R. (2021). The Effects of a Reading-Based Intervention on Emotion Processing in Children Who Have Suffered Early Adversity and War Related Trauma. Frontiers in psychology, 12, 613754. https://doi.org/10.3389/fpsyg.2021.613754
--- experimental_tasks/emotion-recognition/emrec00.m
+++ experimental_tasks/emotion-recognition/emrec00.m
@@ -3,48 +3,27 @@ close all;
 clear;
 sca;
 
-%----------------------------------------------------------------------
-%                         Settings
-%----------------------------------------------------------------------
-scr_width=1366;
-scr_height=768;
+% add custom functions 
+addpath('./functions');
 
 %----------------------------------------------------------------------
 %                       Collect information
 %----------------------------------------------------------------------
 subjectName=input('participant: ','s');
-Gender_folder=input('enter gender of faces in Capital (F or M): ', 's');
+Gender_folder=upper(input('enter gender of faces (F or M): ', 's'));
 
+if ~ (strcmp(Gender_folder,'M') || strcmp(Gender_folder,'F'))
+    error('unknown string for face gender (should be either M or F)');
+end
 
 %----------------------------------------------------------------------
 %                       Task settings
 %----------------------------------------------------------------------
 
-img_format='.png';  %the format of your image files
-bgd_col=255/2;  %background color - set to gray
-fix_disp=0;     %position of fixation point on vertical axis. currently at center of screen
-stimulus_ontime=10;  %max amount of time the stimulus is on screen. I'm setting this equal to the max response time, but could be set lower (in the mail we said 2 sec)
-response_readtime=10; % max amount of time available to respond before next trial starts 
-mask_ontime=1;  %amount of time the mask in on screen
-
-
-%----------------------------------------------------------------------
-%                       Stimuli
-%----------------------------------------------------------------------
-
-%load images and assign handles
-number_images=21;
-for img_i=1:number_images
-    
-    img_path=fullfile(cd,'img',Gender_folder,['Fig_',num2str(img_i),img_format]);
-    img=imread(img_path);
-    img_height=size(img,1);
-    img_width=size(img,2);
-    
-    img_s(img_i) = Screen('openoffScreenwindow',-1,0,[0 0 img_width img_height]);
-    Screen('PutImage',img_s(img_i),img,[0 0 img_width img_height]);
-    
-end
+stim_dur = 10;  % max amount of time the stimulus is on screen (seconds)
+mask_dur = 1;   % amount of time the mask in on screen
+repetitions = 2; % how many repetitions per image
+iti = 1; % inter trial interval
 
 %----------------------------------------------------------------------
 %                       Initialize PTB
@@ -52,6 +31,9 @@ end
 
 % Setup PTB with some default values
 PsychDefaultSetup(2);
+
+% Skip sync tests for demo purposes only
+Screen('Preference', 'SkipSyncTests', 2);
 
 % Seed the random number generator. Here we use the an older way to be
 % compatible with older systems. Newer syntax would be rng('shuffle'). Look
@@ -85,20 +67,31 @@ topPriorityLevel = MaxPriority(window);
 % Get the centre coordinate of the window
 [xCenter, yCenter] = RectCenter(windowRect);
 
+% Get the heigth and width of screen [pix]
+[xRes, yRes] = Screen('WindowSize', window); 
+
 % Set the blend funciton for the screen
 Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 
-
 %----------------------------------------------------------------------
-%                       Timing Information
+%                       Stimuli
 %----------------------------------------------------------------------
 
-% Interstimulus interval time in seconds and frames
-isiTimeSecs = 1;
-isiTimeFrames = round(isiTimeSecs / ifi);
+% generate a pseudorandom trial list
+number_images=21; % number of morhped images
+trial_matrix = combvec(1:repetitions, 1:number_images)';
+trial_matrix = trial_matrix(randperm(size(trial_matrix,1)),:);
+n_trials = number_images*repetitions;
 
-% Numer of frames to wait before re-drawing
-waitframes = 1;
+% fixation
+fix_size = 30;
+
+% image width 
+image_width = 500;
+
+% define coordinates of wherer to present image
+% images are 355 x 464 pixels
+img_rect = CenterRectOnPoint([0,0, image_width, round(464/355 * image_width) ], xCenter, yCenter);
 
 
 %----------------------------------------------------------------------
@@ -109,18 +102,89 @@ waitframes = 1;
 % and right arrow keys as response keys for the task and the escape key as
 % a exit/reset key
 escapeKey = KbName('ESCAPE');
+happyKey = KbName('H');
+sadKey = KbName('S');
 
 
+%----------------------------------------------------------------------
+%                 Prepare for saving data
+%----------------------------------------------------------------------
+
+% Make a directory for the results
+resultsDir = [pwd '/data/'];
+if exist(resultsDir, 'dir') < 1
+    mkdir(resultsDir);
+end
+
+% prep data header
+datFid = fopen([resultsDir subjectName], 'w');
+fprintf(datFid, 'id_n\tgender\timg_name\tmorph_level\tresp_happy\tRT\n');
 
 %----------------------------------------------------------------------
 %                       Experimental loop
 %----------------------------------------------------------------------
 
-% Animation loop: we loop for the total number of trials
-for trial = 1:numTrials
+DrawFormattedText(window, 'Welcome to our experiment \n\n Please indicate by pressing H or S if the faces you see are happy or sad, respectively \n\n Press Any Key To Start',...
+    'center', 'center', black);
+Screen('Flip', window);
+KbStrokeWait;
 
-   
+HideCursor; % hide mouse cursor
+
+% Animation loop: we loop for the total number of trials
+for t = 1:n_trials
+    
+    % fixation spot
+    Screen('FillOval', window, black, CenterRectOnPoint([0,0, round(fix_size), round(fix_size)], xCenter, yCenter));
+    Screen('Flip', window);
+    WaitSecs(iti/2);
+    
+    % load image
+    img_path = [pwd '/img/' upper(Gender_folder) '/Fig_' num2str(trial_matrix(t,2)) '.png'];
+    img = imread(img_path);
+    face_tex = Screen('MakeTexture', window, img);
+    
+    % generate noise texture
+    scrambled_img = imscramble(img,0.9, 'cutoff');
+    noise_tex = Screen('MakeTexture', window, scrambled_img);
+    
+    % put stimulus on screen
+    Screen('DrawTexture', window, face_tex, [], img_rect);
+    [~,t0] = Screen('Flip', window);
+    
+    % wait for response or until time run out
+    resp_happy = NaN;
+    while (GetSecs-t0) <= stim_dur
+        [keyisdown, secs, keycode] = KbCheck(-1);
+        if keyisdown && (keycode(happyKey) || keycode(sadKey))
+            tResp = secs - t0;
+            if keycode(happyKey)
+                resp_happy = 1;
+            else
+                resp_happy = 0;
+            end
+            break;
+        end
+    end
+    
+    % present mask
+    Screen('DrawTexture', window, noise_tex, [], img_rect);
+    [~,tmask] = Screen('Flip', window);
+    % WaitSecs(mask_dur);
+    
+    % write data line to file
+    dataline = sprintf('%s\t%s\t%s\t%i\t%i\t%2f\n', subjectName, Gender_folder, ['Fig_' num2str(trial_matrix(t,2)) '.png'], trial_matrix(t,2), resp_happy, tResp);
+    fprintf(datFid, dataline);
+    
+    % back to fixation spot
+    Screen('FillOval', window, black, CenterRectOnPoint([0,0, round(fix_size), round(fix_size)], xCenter, yCenter));
+    Screen('Flip', window, tmask + mask_dur);
+    WaitSecs(iti/2);
+
 end
+
+% close data file
+fclose(datFid);
 
 % End of experiment screen. We clear the screen once they have made their
 % response
